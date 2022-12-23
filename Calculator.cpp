174f8b25dc75ffbd9499859d1d805df355ae155e
dohadoah <yidohaa@gmail.com>
2022-12-23 17:53:43 +0900

    Update Calculator.cpp

--- Calculator.cpp
+++ Calculator.cpp
@@ -6,17 +6,22 @@ int main()
 {
     int a;
     int b;
+    string opt;
     int sum;
 
-    cout << "Enter a number hoss! \n";
+    cout << "Enter first number: \n";
     cin >> a;
 
-    cout << "Enter another number \n";
+    cout << "Enter a Operator: \n";
+    cin >> opt;
+
+
+    cout << "Enter another number: \n";
     cin >> b;
 
     sum = a + b;
     cout << "the sum of those numbers is " << sum << endl;
-    
+
 
 
     return 0;
