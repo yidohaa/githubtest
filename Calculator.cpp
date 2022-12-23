347389f06e78c104854379fe4d3bc7d4e6778805
dohadoah <yidohaa@gmail.com>
2022-12-23 17:55:58 +0900

    Update Calculator.cpp

--- Calculator.cpp
+++ Calculator.cpp
@@ -4,21 +4,25 @@ using namespace std;
 
 int main()
 {
-    int a;
-    int b;
+    int num1, num2;
+    char op;
+
+
+
+
     string opt;
     int sum;
 
     cout << "Enter first number: \n";
-    cin >> a;
+    cin >> num1;
 
     cout << "Enter a Operator: \n";
     cin >> opt;
 
-
     cout << "Enter another number: \n";
-    cin >> b;
+    cin >> num2;
 
+    if "opt" == "+":
     sum = a + b;
     cout << "the sum of those numbers is " << sum << endl;
 
