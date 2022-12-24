c149d30aded7e02baa4a45a5a2c8572d0748a920
dohadoah <yidohaa@gmail.com>
2022-12-24 16:13:46 +0900

    new

---
+++ Guesswhat.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int secretNum = 7;
+    int guessedNum;
+
+    while (secretNum != guessedNum){
+        cout << "Enter guess number:";
+        cin >> guessedNum;
+
+
+    }
+
+    cout << "You Win!";
+
+    return 0;
+
+
+}
