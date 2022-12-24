4890c5a2069f671196be37f3ec51fecfee76ce79
dohadoah <yidohaa@gmail.com>
2022-12-24 21:46:02 +0900

    listpractice

--- Untitled-1.cpp
+++ Untitled-1.cpp
@@ -1,13 +1,34 @@
-#include<iostream>
-
+#include <iostream>
+#include <list>
+#include <string>
 using namespace std;
 
-char name;
-
-int main()
+int main() 
 {
-    cout << "name:";
-    cin >> name;
-    cout << name;
+	//declaring a list
+	list<string> lstr;
+	//declaring iterator to the list
+	list<string>::iterator l_iter;
+	
+	//declaring string object
+	string str;
 
+	//input strings
+	while(true)
+	{
+		cout<<"Enter string (\"EXIT\" to quit): ";
+		getline(cin, str);
+		if(str=="EXIT")
+			break;
+
+		//adding string to the list 
+		lstr.push_back(str);
+	}
+
+	//printing list elements
+	cout<<"List elements are"<<endl;
+	for (l_iter = lstr.begin(); l_iter != lstr.end(); l_iter++)
+		cout<< *l_iter<<endl;
+
+	return 0;
 }
