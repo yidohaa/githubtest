641ba5b8df038cec61c4802b8267c128934e8468
dohadoah <yidohaa@gmail.com>
2022-12-24 22:02:46 +0900

    ddd

--- listpractice.cpp
+++ listpractice.cpp
@@ -1,3 +1,4 @@
+'''
 #include<iostream.h>
 #include<list.h>
 Using namespace std;
@@ -14,4 +15,33 @@ int main ( ){
    for( x=list.begin( ); x != list.end( ); ++x)
       cout<< “ “<<*x;
    return 0;
+}
+'''
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[10], tot=10, i, elem, j, found=0;
+    cout<<"Enter 10 Array Elements: ";
+    for(i=0; i<tot; i++)
+        cin>>arr[i];
+    cout<<"\nEnter Element to Delete: ";
+    cin>>elem;
+    for(i=0; i<tot; i++)
+    {
+        if(arr[i]==elem)
+        {
+            for(j=i; j<(tot-1); j++)
+                arr[j] = arr[j+1];
+            found++;
+            i--;
+            tot--;
+        }
+    }
+    if(found==0)
+        cout<<"\nElement doesn't found in the Array!";
+    else
+        cout<<"\nElement Deleted Successfully!";
+    cout<<endl;
+    return 0;
 }
