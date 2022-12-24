c35e546541c983cb29c0abe313e60d1167ea7631
dohadoah <yidohaa@gmail.com>
2022-12-24 16:37:30 +0900

    dddd

--- Guesswhat.cpp
+++ Guesswhat.cpp
@@ -5,21 +5,37 @@ int main()
 {
     int secretNum = 7;
     int guessedNum;
+    char yesorno;
+
+
+    cout << "Do you want to play Guesswhat game?" << endl << "y/n";
+    cin >> yesorno;
+    if (yesorno == 'y'){
+
+
+        
+    }
+
+
+
 
     while (secretNum != guessedNum){
         cout << "Enter guess number:";
         cin >> guessedNum;
-        if(guessedNum >= secretNum){
-            cout << "Secret number is greater than your guess number";
+        if(guessedNum > secretNum){
+            cout << "Secret number is greater than your guess number" << endl;
         }
-        else if(guessedNum <= secretNum){
-            cout << "Secret number is less than your guess number";
+        else if(guessedNum < secretNum){
+            cout << "Secret number is less than your guess number" <<endl;
+        }
+        else if(guessedNum == secretNum){
+            cout << "Secret number is equal to your guess number " << endl;
+            cout << "Congratulation, You Win.";
         }
         
 
     }
 
-    cout << "You Win!";
 
     return 0;
 
