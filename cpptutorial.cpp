cc71070fa3e334ce003ec594a24578872264d192
dohadoah <yidohaa@gmail.com>
2023-01-01 14:28:04 +0900

    333

--- cpptutorial.cpp
+++ cpptutorial.cpp
@@ -1,19 +1,19 @@
+//STRINGSTREAMS
 #include<iostream>
+#include<string>
 using namespace std;
 
 
 int main()
 {
-    int i;
-    cout << "Please enter an integer value:";
-    cin >> i;
-
-    cout << "The value you entered is " << i;
-    cout << " and its double is " << i*2 << ".\n";
+    string mystr;
+    cout << "What\'s your name?";
+    getline (cin, mystr);
+    cout << "hello " << mystr << ".\n";
+    cout << "What\'s your favorite sports??";
+    getline (cin, mystr);
+    cout << "I like " << mystr << " too!\n";
     return 0;
 
-
-
-    return 0;
 
 }
